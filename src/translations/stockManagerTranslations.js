3792b31d0b568ba62c4e52bae53a6a952673f050
Nahuel <nahuelguirao123@gmail.com>
2024-05-02 12:02:51 -0300

    Actualizo cv y medias queries

--- src/translations/stockManagerTranslations.js
+++ src/translations/stockManagerTranslations.js
@@ -4,7 +4,7 @@ export const stockManagerTranslations = {
         technologies: "Tecnologías utilizadas"
     },
     en: {
-        description: "Simple stock management system where users can create, edit, and filter products. The client is built with React + TypeScript along with a responsive design. The API is constructed with Express + PostgreSQL. Finally, the app has been deployed on Vercel.",
+        description: "Simple stock management system where users can create, edit and filter products. The client is built with React + TypeScript along with a responsive design. The API is constructed with Express + PostgreSQL. Finally, the app has been deployed on Vercel.",
         technologies: 'Used technologies'
     }
 }
